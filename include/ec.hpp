23d60af26dece96ea61085157b1664b877d3cbe5
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2012-12-07 09:56:51 +0100

    Sc refinement - Genode specific
    Delete SC solely, if it is running in the context of its associated EC.
    
    Pro: Avoid deadlocks if a worker EC holds a lock and looses the SC it gets
         runtime from. If never ever a new SC is helping the worker EC, the lock
         is never freed up. Another worker EC trying to get the very same lock
         will get be stucked forever.
    
    Con: A global EC can manage to prevent that the SC gets deleted inside the
         kernel by not running the SC in the context of the global EC.
    	 (e.g. a worker thread that would never reply to the global thread).

--- include/ec.hpp
+++ include/ec.hpp
@@ -284,7 +284,7 @@ class Ec : public Kobject, public Refcount, public Queue<Sc>
             Lock_guard <Spinlock> guard (lock);
 
             for (Sc *s; dequeue (s = head()); ) {
-                if (EXPECT_FALSE(s->del_ref())) {
+                if (EXPECT_FALSE(s->del_ref()) && (this == s->ec)) {
                     delete s;
                     continue;
                 }
