8fdaea6612bdee502b73de3bbf17e3fb5631d534
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2017-09-07 23:05:54 +0200

    Track execution time caused by cross core IPC

--- include/syscall.hpp
+++ include/syscall.hpp
@@ -191,6 +191,9 @@ class Sys_sc_ctrl : public Sys_regs
         inline unsigned long sc() const { return ARG_1 >> 8; }
 
         ALWAYS_INLINE
+        inline unsigned op() const { return flags() & 0x1; }
+
+        ALWAYS_INLINE
         inline void set_time (uint64 val)
         {
             ARG_2 = static_cast<mword>(val >> 32);
